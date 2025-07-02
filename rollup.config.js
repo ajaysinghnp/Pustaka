c7eea73a5e84ad007fafebe474f83466774310ea
Ajay Singh <admin@ajaysingh.com.np>
2025-07-02 21:11:48 +0545

    rollup setup complete

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "packages/core/src/index.ts",
+  output: [
+    {
+      file: "dist/pustaka.umd.js",
+      format: "umd",
+      name: "Pustaka"
+    },
+    {
+      file: "dist/pustaka.esm.js",
+      format: "esm"
+    }
+  ],
+  plugins: [nodeResolve(), typescript()]
+};
