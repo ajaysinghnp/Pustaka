e5405cec75c5c2ab74282e0bb9a291f8723f0932
Ajay Singh <admin@ajaysingh.com.np>
2025-07-09 19:18:20 +0545

    rollup config cleaned up for old pdf lib

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import type { RollupOptions } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import alias from '@rollup/plugin-alias';
-import { fileURLToPath } from 'node:url';
-
-// Equivalent of __filename and __dirname in ES modules:
-const __pdf_worker = fileURLToPath(
-  new URL('node_modules/pdfjs-dist/build/pdf.worker.min.mjs', import.meta.url),
-);
 
 const config: RollupOptions = {
   input: 'packages/core/index.ts',
@@ -29,18 +22,7 @@ const config: RollupOptions = {
       sourcemap: true,
     },
   ],
-  plugins: [
-    alias({
-      entries: [
-        {
-          find: 'pdfjs-dist/build/pdf.worker.entry.js',
-          replacement: __pdf_worker,
-        },
-      ],
-    }),
-    nodeResolve(),
-    typescript(),
-  ],
+  plugins: [nodeResolve(), typescript()],
   // external: Object.keys(pkg.dependencies),
 };
 
